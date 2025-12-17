35b1da0f9345a20446174f49e4d49bcfcc7dc09c
Veljko Stojanovic <veljkostojanovic@Veljkos-Air.netis.cc>
2025-12-18 00:12:54 +0100

    resavanje merge konflikta

--- services/notifications/routes.go
+++ services/notifications/routes.go
@@ -2,10 +2,7 @@ package main
 
 import (
 	"spotiftn/notifications/handlers"
-<<<<<<< HEAD
-=======
 	"spotiftn/notifications/middleware"
->>>>>>> main
 
 	"github.com/gin-gonic/gin"
 )
@@ -13,18 +10,10 @@ import (
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
 
-<<<<<<< HEAD
-	r.GET("/:userID", handlers.GetNotifications)
-	r.POST("/", handlers.CreateNotification)
-
-=======
->>>>>>> main
 	r.GET("/health", func(c *gin.Context) {
 		c.JSON(200, gin.H{"status": "OK", "service": "notifications"})
 	})
 
-<<<<<<< HEAD
-=======
 	api := r.Group("/")
 	api.Use(middleware.AuthMiddleware())
 	{
@@ -32,6 +21,5 @@ func SetupRouter() *gin.Engine {
 		api.POST("/", handlers.CreateNotification)
 	}
 
->>>>>>> main
 	return r
 }
