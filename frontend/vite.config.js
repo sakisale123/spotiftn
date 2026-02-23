b3fbfa26ab27861bfe8ca45cf55895bf73dd5866
salepopov <salepopov107@gmail.com>
2026-02-23 06:51:32 +0100

    Complete HTTPS

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import fs from 'fs';
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -11,5 +13,9 @@ export default defineConfig({
     host: true,
     strictPort: true,
     port: 5173,
+    https: {
+      key: fs.readFileSync('/etc/ssl/certs/server.key'),
+      cert: fs.readFileSync('/etc/ssl/certs/server.crt'),
+    },
   }
 })
